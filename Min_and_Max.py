985a6b295d694284d6621812212f8ed46891cfb6
SalihTasdelen <salih.tasdelen@hotmail.com>
2020-06-16 16:16:40 +0300

    Min and Max

---
+++ Min_and_Max.py
@@ -0,0 +1,7 @@
+import numpy as np
+temp = list(map(int, input().split()))
+n, m = temp[0], temp[1]
+arr = np.empty((n,m), dtype="int32")
+for i in range(n):
+    arr[i] = list(map(int, input().split()))
+print(np.max(np.min(arr,axis=1)))
